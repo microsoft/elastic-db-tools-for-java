c70fef3c0d2f9b16ddea8165145c2aa66159bd25
virtuositycg <muru@virtuositycg.com>
2017-04-06 04:55:39 -0700

    Changes made on conversion of XElement to JAXBElement

--- azure-elasticdb-shard/src/main/java/com/microsoft/azure/elasticdb/shard/storeops/base/StoreOperationInput.java
+++ azure-elasticdb-shard/src/main/java/com/microsoft/azure/elasticdb/shard/storeops/base/StoreOperationInput.java
@@ -1,13 +1,20 @@
 package com.microsoft.azure.elasticdb.shard.storeops.base;
 
+import com.microsoft.azure.elasticdb.shard.base.ShardKey;
 import com.microsoft.azure.elasticdb.shard.base.ShardLocation;
+import com.microsoft.azure.elasticdb.shard.base.ShardRange;
+import com.microsoft.azure.elasticdb.shard.store.IStoreMapping;
+import com.microsoft.azure.elasticdb.shard.store.IStoreShard;
 import com.microsoft.azure.elasticdb.shard.store.IStoreShardMap;
 import com.microsoft.azure.elasticdb.shard.utils.GlobalConstants;
 
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
 import java.util.UUID;
 
+@XmlRootElement
 public class StoreOperationInput {
 
     @XmlElement(name = "GsmVersion")
@@ -27,6 +34,36 @@ public class StoreOperationInput {
 
     @XmlElement(name = "Location")
     private ShardLocation location;
+    
+    @XmlElement(name = "OperationCode")
+    private StoreOperationCode operationCode;
+    
+    @XmlElement(name = "Undo")
+    private boolean undo;
+    
+    @XmlElement(name = "Shard")
+    private IStoreShard shard;
+    
+    @XmlElement(name = "ShardOld")
+    private IStoreShard shardOld;
+    
+    @XmlElement(name = "ShardRange")
+    private ShardRange range;
+    
+    @XmlElement(name = "ShardKey")
+    private ShardKey key;
+    
+    @XmlElement(name = "Mapping")
+    private IStoreMapping mapping;
+    
+    @XmlElement(name = "MappingTarget")
+    private IStoreMapping mappingTarget;
+    
+    @XmlElement(name = "LockOwnerId")
+    private UUID lockOwnerId;
+    
+    @XmlElement(name = "PatternForKill")
+    private String patternForKill;
 
     private StoreOperationInput(){}
 
@@ -62,6 +99,57 @@ public class StoreOperationInput {
             input.location = location;
             return this;
         }
+        
+        public Builder withStoreOperationCode(StoreOperationCode operationCode){
+        	input.operationCode = operationCode;
+        	return this;
+        }
+        
+        public Builder withUndo(boolean undo){
+        	input.undo = undo;
+        	return this;
+        }
+        
+        public Builder withIStoreShard(IStoreShard shard){
+        	input.shard = shard;
+        	return this;
+        }
+        
+        public Builder withIStoreShardOld(IStoreShard shardOld){
+        	input.shardOld = shardOld;
+        	return this;
+        }
+        
+        public Builder withShardRange(ShardRange range){
+        	input.range = range;
+        	return this;
+        }
+        
+        public Builder withShardKey(ShardKey key){
+        	input.key = key;
+        	return this;
+        }
+        
+        public Builder withIStoreMapping(IStoreMapping mapping){
+        	input.mapping = mapping;
+        	return this;
+        }
+        
+        public Builder withIStoreMappingTarget(IStoreMapping mappingTarget){
+        	input.mappingTarget = mappingTarget;
+        	return this;
+        }
+        
+        public Builder withLockOwnerId(UUID lockOwnerId){
+        	input.lockOwnerId = lockOwnerId;
+        	return this;
+        }
+        
+        public Builder withPatternForKill(String patternForKill){
+        	input.patternForKill = patternForKill;
+        	return this;
+        }
+        
         public StoreOperationInput build() {
             return input;
         }
