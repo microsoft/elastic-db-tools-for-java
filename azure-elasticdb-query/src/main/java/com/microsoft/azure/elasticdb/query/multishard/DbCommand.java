224ab0119b05d26ee5b6d7b43064a82b891b5f62
Dhruva <dhruva.n.murthy@outlook.com>
2017-04-10 08:05:47 +0530

    MultiShard Implementation

---
+++ azure-elasticdb-query/src/main/java/com/microsoft/azure/elasticdb/query/multishard/DbCommand.java
@@ -0,0 +1,27 @@
+package com.microsoft.azure.elasticdb.query.multishard;
+
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+import com.microsoft.azure.elasticdb.shard.utils.ICloneable;
+import com.microsoft.sqlserver.jdbc.SQLServerConnection;
+
+public class DbCommand implements ICloneable {
+
+    //TODO: mimic .NET's System.Data.Common.DbCommand
+
+    private SQLServerConnection connection;
+
+    public SQLServerConnection getConnection() {
+        return this.connection;
+    }
+
+    public void setConnection(SQLServerConnection connection) {
+        this.connection = connection;
+    }
+
+    @Override
+    public DbCommand Clone() {
+        return new DbCommand();
+    }
+}
